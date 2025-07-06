abd5094f429e81ec1cb3b70f05ed01b21aaaace1
Richie <mcrich.md@gmail.com>
2025-07-06 07:44:04 +0100

    more styling and functionality

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ const down = ()=>{
 }
 document.onkeydown = up;
 document.onkeyup = down;
+// the code below prevents the projects from being seen till the nav-project is clicked.
+let projects = document.getElementById('project');
+let navProject = document.getElementById('navProject');
+let closeButton = document.getElementById('closeProjects')
+projects.hidden = true;
+const showProjects =()=>{
+    projects.hidden = false;
+}
+navProject.onclick = showProjects;
+
+const closeProjects =()=>{
+    projects.hidden = true;
+}
+closeButton.onclick = closeProjects;
